7b478110c2c71e1be210f67b8e70aa756f28327c
faridg <faridg@twc.com>
2018-01-29 07:05:27 -0500

    display post author

--- index.php
+++ index.php
@@ -16,6 +16,9 @@
 	<?php if(have_posts()) : ?>
 		<?php while(have_posts()): the_post(); ?>
 			<h3><?php the_title(); ?></h3>
+			<div class="meta">
+			Crated By <?php the_author(); ?>
+			</div>
 			<?php the_content(); ?>
 		<?php endwhile?>
 	<?php else : ?>
