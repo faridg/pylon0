6f72f43415520b1d7cf3f263ab0902f71fac637a
faridg <faridg@twc.com>
2018-01-29 09:31:10 -0500

    nothing major

--- index.php
+++ index.php
@@ -3,15 +3,21 @@
 		<div class="container">
 		<?php if(have_posts()) : ?>
 			<?php while(have_posts()): the_post(); ?>
-				<h3>
-				<a href="<?php the_permalink(); ?>">
-				<?php the_title(); ?>
+				<article class="post">
+					<h3>
+					<a href="<?php the_permalink(); ?>">
+					<?php the_title(); ?>
+					</a>
+					</h3>
+					<div class="meta">
+					Created By <?php the_author(); ?> on <?php the_date(); ?>
+					</div>
+					<?php the_content(); ?>
+				<br>
+				<a class="read-more" href="<?php the_permalink(); ?>">
+				read more
 				</a>
-				</h3>
-				<div class="meta">
-				Created By <?php the_author(); ?> on <?php the_date(); ?>
-				</div>
-				<?php the_content(); ?>
+				</article>
 			<?php endwhile?>
 		<?php else : ?>
 			<?php echo wpautop('add some posts, nothing found'); ?>
