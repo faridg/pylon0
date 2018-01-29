2ef3a6f20d63a708d0982c7940ecb03e9ac5a113
faridg <faridg@twc.com>
2018-01-29 08:11:23 -0500

    minor changes

--- index.php
+++ index.php
@@ -8,25 +8,32 @@
 </head>
 <body>
 	<header>
+		<div class ="container">
 		<h1><?php bloginfo('name'); ?></h1>
 		<span><?php bloginfo('description'); ?></span>
+		</div>
 	</header>
 	
 	<div class="main">
-	<?php if(have_posts()) : ?>
-		<?php while(have_posts()): the_post(); ?>
-			<h3><?php the_title(); ?></h3>
-			<div class="meta">
-			Crated By <?php the_author(); ?> on <?php the_date(); ?>
-			</div>
-			<?php the_content(); ?>
-		<?php endwhile?>
-	<?php else : ?>
-		<?php echo wpautop('add some posts, nothing found'); ?>
-	<?php endif; ?>
+		<div class="container">
+		<?php if(have_posts()) : ?>
+			<?php while(have_posts()): the_post(); ?>
+				<h3><?php the_title(); ?></h3>
+				<div class="meta">
+				Created By <?php the_author(); ?> on <?php the_date(); ?>
+				</div>
+				<?php the_content(); ?>
+			<?php endwhile?>
+		<?php else : ?>
+			<?php echo wpautop('add some posts, nothing found'); ?>
+		<?php endif; ?>
+		</div>
 	</div>
 	<footer>
-	<p>&copy; <?php the_date('Y'); ?> - <?php bloginfo('name'); ?></p>
+		<div class="container">
+			<p>&copy; <?php the_time('Y'); ?> - <?php bloginfo('name'); ?></p>
+		</div>
 	</footer>
+	<?php wp_footer(); ?>
 </body>
 </html>
