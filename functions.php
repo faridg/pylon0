81f44d396c74297f095505327d65c4275b6ffe48
faridg <faridg@twc.com>
2018-01-29 09:58:27 -0500

    excerpt lenght

--- functions.php
+++ functions.php
@@ -1,7 +1,12 @@
 <?php
-function pylon_theme_setup(){
+function pylon_featured_image(){
 // Include Featured Image Option
-add_theme_support('post-thumbnails');
+add_theme_support('featured_image');
 }
-add_action('after_setup_theme', 'pylon_theme_setup');
+add_action('after_setup_theme', 'pylon_featured_image');
+// Post length
+function post_length(){
+	return 24;
+}
+add_filter ('excerpt_length', 'post_length');
 ?>
