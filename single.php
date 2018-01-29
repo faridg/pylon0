b9a90c9bff54050c09d636a1f9e870a29db7df37
faridg <faridg@twc.com>
2018-01-29 09:30:56 -0500

    new single page

---
+++ single.php
@@ -0,0 +1,23 @@
+<?php get_header(); ?>	
+	<div class="main">
+		<div class="container">
+		<?php if(have_posts()) : ?>
+			<?php while(have_posts()): the_post(); ?>
+				<article class="post">
+					<h3>
+					<a href="<?php the_permalink(); ?>">
+					<?php the_title(); ?>
+					</a>
+					</h3>
+					<div class="meta">
+					Created By <?php the_author(); ?> on <?php the_date(); ?>
+					</div>
+					<?php the_content(); ?>
+				</article>
+			<?php endwhile?>
+		<?php else : ?>
+			<?php echo wpautop('add some posts, nothing found'); ?>
+		<?php endif; ?>
+		</div>
+	</div>
+<?php get_footer(); ?>
